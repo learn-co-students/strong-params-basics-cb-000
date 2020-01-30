69cf50fe982b54ecf1050e7dd37814864d8e22cb
s-blais <stephen@s-blais.com>
2020-01-30 16:04:06 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,30 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+		# @post = Post.new(params["post"])
+		# @post = Post.new(params.require(:post).permit(:title, :description))
+		@post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+		# @post.update(params["post"])
+		# @post.update(params.require(:post).permit(:title))
+		@post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+ 
+	# the below takes the desired param keys as an argument from the create or update methods, then creates a permit list from that argument, so post_params can return customized permitted params to the method.
+	def post_params(*args)
+		params.require(:post).permit(*args)
+	end
+
 end
